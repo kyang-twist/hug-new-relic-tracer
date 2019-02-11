9bf046144cc3064df4e302d7e5d7843d031f6a6f
Kevin Yang <kyang@twistbioscience.com>
2019-02-11 12:53:58 -0800

    fix typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-  name='hug-new-relic-tracer'
+  name='hug-new-relic-tracer',
   version='1.0.0',
   packages=find_packages(),
   url='https://github.com/kyang-twist/hug-new-relic-tracer',
