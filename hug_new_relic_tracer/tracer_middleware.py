6a290776d79d0c142ca4c5866d9a5cd7266db454
Kevin Yang <kyang@twistbioscience.com>
2019-02-11 17:55:42 -0800

    move interaction to post-response to make it work

--- hug_new_relic_tracer/tracer_middleware.py
+++ hug_new_relic_tracer/tracer_middleware.py
@@ -5,5 +5,5 @@ class TracerMiddleware:
   def get_transaction_name(self, req) -> str:
     return f'{req.method} {req.relative_uri}'
 
-  def process_request(self, req, resp):
-    newrelic_agent.set_transaction_name(self.get_transaction_name(req))
+  def process_response(self, req, resp, resource, req_succeeded):
+    newrelic_agent.set_transaction_name(self.get_transaction_name(req), priority=100)
