5448169478f68c8ef0609cf531abe43ece78db72
Kevin Yang <kyang@twistbioscience.com>
2019-02-11 18:51:23 -0800

    print uri template instead of full url

--- hug_new_relic_tracer/tracer_middleware.py
+++ hug_new_relic_tracer/tracer_middleware.py
@@ -3,7 +3,7 @@ import newrelic.agent as newrelic_agent
 class TracerMiddleware:
   
   def get_transaction_name(self, req) -> str:
-    return f'{req.method} {req.relative_uri}'
+    return f'{req.method} {req.uri_template}'
 
   def process_response(self, req, resp, resource, req_succeeded):
     newrelic_agent.set_transaction_name(self.get_transaction_name(req), priority=100)
